76dabcea7eb714626536fd2e8083a31536e9dcf5
Sandra <sandraa_03@hotmail.es>
2016-10-26 12:38:44 +0200

    Tutorial 1

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /media/sandra/Sandra 1TB/UVic/Trimestre1/IntegracioRobotica/Unitat2/ROS_Tutorials/catkin_ws/src
+# Build directory: /media/sandra/Sandra 1TB/UVic/Trimestre1/IntegracioRobotica/Unitat2/ROS_Tutorials/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ catkin_ws/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /media/sandra/Sandra 1TB/UVic/Trimestre1/IntegracioRobotica/Unitat2/ROS_Tutorials/catkin_ws/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
